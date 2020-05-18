9d122bc9dbf45425eda161e01497f62614fd19a8
Lion-Ga92 <Luis@tamalesgarcia.com>
2020-05-17 21:12:37 -0700

    this is the final test of proto task app

--- task_list.py
+++ task_list.py
@@ -1,3 +1,5 @@
+from sys import exit
+
 print("Welcome to our script, valid inputs are Complete and Not Complete")
 print("To exit app you must type 'quit'")
 
@@ -5,25 +7,52 @@ print("To exit app you must type 'quit'")
 def check_list():
     task_1 = "Cut corn"
     task_2 = "Peel corn"
-    global run
+    task_3 = "Wash pots"
+    task_4 = "Clean table"
     task_list = []
     c_t1_check = input("Have you completed task 1? ")
     if c_t1_check == "Complete":
         task_list.append((task_1, c_t1_check))
-        return print(task_list)
+        print(task_list)
     elif c_t1_check == "Not complete":
         task_list.append((task_1, c_t1_check))
-        return print(task_list)
+        print(task_list)
+    elif c_t1_check == "quit":
+        return exit()
+    else:
+        print("Response not valid")
+
     c_t2_check = input("Have you comp. task 2? ")
     if c_t2_check == "Complete":
         task_list.append((task_2, c_t2_check))
-        return print(task_list)
+        print(task_list)
     elif c_t2_check == "Not complete":
         task_list.append((task_2, c_t2_check))
-    if c_t1_check == "quit":
-        run = False
+        print(task_list)
+    elif c_t2_check == "quit":
+        return exit()
+    else:
+        print("Response not valid")
+    c_t3_check = input("Have you completed task 3?? " )
+    if c_t3_check == "Complete":
+        task_list.append((task_3, c_t3_check))
+        print(task_list)
+    elif c_t3_check == "Not complete":
+        task_list.append((task_3, c_t3_check))
+        print(task_list)
+    elif c_t3_check == "quit":
+        return exit()
+    else:
+        print("Response not valid")
+    c_t4_check = input("Have you completed task 4? ")
+    if c_t4_check == "Complete":
+        task_list.append((task_4, c_t4_check))
+    elif c_t4_check == "Not complete":
+        task_list.append((task_4, c_t4_check))
+        print(task_list)
+    elif c_t4_check == "quit":
+        exit()
+    else:
+        print("Response not valid")
 
-
-run = True
-while run:
-    check_list()
+check_list()
